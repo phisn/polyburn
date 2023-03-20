acb4f546c22d6b7deb0f5b34eff9d903e57c370e
phisn <phisn@outlook.de>
2023-03-20 14:09:47 +0100

    Add transition and icons to menu

--- webapp/src/common/components/Dialog.tsx
+++ webapp/src/common/components/Dialog.tsx
@@ -11,16 +11,18 @@ function Dialog(props: DialogProps) {
     return (
         <Transition show={props.open} as={Fragment}>
             <NativeDialog
-                open={props.open} 
                 onClose={() => props.closeDialog()}
                 as="div"
                 className="relative z-10">
 
                 <Transition.Child
                     as={Fragment}
-                    enter="ease-out duration-150"
+                    enter="ease-out duration-200"
                     enterFrom="opacity-0"
                     enterTo="opacity-100"
+                    leave="ease-in duration-100"
+                    leaveFrom="opacity-100"
+                    leaveTo="opacity-0"
                 >
                     <div className="fixed inset-0 bg-black bg-opacity-50" />
                 </Transition.Child>
@@ -29,9 +31,12 @@ function Dialog(props: DialogProps) {
                     <div className="flex min-h-full items-center justify-center p-4">
                         <Transition.Child
                             as={Fragment}
-                            enter="ease-out duration-150"
-                            enterFrom="opacity-0"
-                            enterTo="opacity-100"
+                            enter="ease-out duration-200"
+                            enterFrom="opacity-0 scale-95"
+                            enterTo="opacity-100 scale-100"
+                            leave="ease-in duration-100"
+                            leaveFrom="opacity-100 scale-100"
+                            leaveTo="opacity-0 scale-95"
                         >
                             <NativeDialog.Panel className="w-full max-w-lg bg-base-100 p-6 rounded-xl space-y-4">
                                 { props.children }

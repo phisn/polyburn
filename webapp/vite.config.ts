222aad3e3155f66d18409576639154fd54bf9a0a
phisn <phisn@outlook.de>
2023-03-25 00:58:53 +0100

    Add Icon & Make installable

--- webapp/vite.config.ts
+++ webapp/vite.config.ts
@@ -1,9 +1,26 @@
 import react from "@vitejs/plugin-react"
 import { defineConfig } from "vite"
+import { VitePWA } from "vite-plugin-pwa"
 
-// https://vitejs.dev/config/
 export default defineConfig({
     plugins: [
         react(),
+        VitePWA({ 
+            registerType: "autoUpdate",
+            devOptions: {
+                enabled: true,
+            },
+            manifest: {
+                name: "Rocket Game",
+                theme_color: "#000000",
+                icons: [
+                    {
+                        src: "/src/assets/icon.png",
+                        sizes: "512x512",
+                        type: "image/png",
+                    }
+                ]
+            }
+        })
     ]
 })

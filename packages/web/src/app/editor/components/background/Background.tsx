b1a54d83f8eb7fb77b376de73d7fd0ff62b9b9f9
phisn <phisn@outlook.de>
2023-08-10 00:29:07 +0200

    Refactor editor background

--- packages/web/src/app/editor/components/background/Background.tsx
+++ packages/web/src/app/editor/components/background/Background.tsx
@@ -1,17 +1,9 @@
 import { Html } from "@react-three/drei"
 import { useState } from "react"
-import { EntityType } from "runtime/src/core/common/EntityType"
-import { entityModelRegistry } from "runtime/src/model/world/EntityModelRegistry"
 import { Point } from "runtime/src/model/world/Point"
-import { ContextMenu } from "../../../../common/components/ContextMenu"
-import { FlagSvg } from "../../../../common/components/inline-svg/Flag"
-import { RocketSvg } from "../../../../common/components/inline-svg/Rocket"
-import { TriangleSvg } from "../../../../common/components/inline-svg/Triangle"
-import { levelNew } from "../../entities/level/mutations/levelNew"
-import { rocketNew } from "../../entities/rocket/mutations/rocketNew"
-import { shapeNew } from "../../entities/shape/mutations/shapeNew"
 import { useEditorStore } from "../../store/EditorStore"
 import { ConsumeEvent, Priority, useEventListener } from "../../store/EventStore"
+import { CreateEntityMenu } from "./CreateEntityMenu"
 
 interface ModeNone {
     type: "none"
@@ -54,60 +46,13 @@ export function Background() {
     }, priority)
 
     if (mode.type === "contextMenu") {
-        function spawnPositionForType(position: Point, type: EntityType) {
-            return {
-                x: position.x - entityModelRegistry[EntityType.Rocket].width / 2,
-                y: position.y + entityModelRegistry[EntityType.Rocket].height / 2,
-            }
-        }
-
         return (
             <Html as="div" position={[mode.position.x, mode.position.y, priority]}>
-                <ContextMenu>
-                    <li>
-                        <a
-                            onClick={() => {
-                                dispatchMutation(shapeNew({ ...mode.position }))
-                                setMode({ type: "none" })
-                            }}
-                        >
-                            <TriangleSvg width="16" height="16" />
-                            Create Shape
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            onClick={() => {
-                                dispatchMutation(
-                                    rocketNew(
-                                        spawnPositionForType(mode.position, EntityType.Rocket),
-                                        0,
-                                    ),
-                                )
-                                setMode({ type: "none" })
-                            }}
-                        >
-                            <RocketSvg width="16" height="16" />
-                            Create Rocket
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            onClick={() => {
-                                dispatchMutation(
-                                    levelNew(
-                                        spawnPositionForType(mode.position, EntityType.Level),
-                                        0,
-                                    ),
-                                )
-                                setMode({ type: "none" })
-                            }}
-                        >
-                            <FlagSvg width="16" height="16" />
-                            Create Level
-                        </a>
-                    </li>
-                </ContextMenu>
+                <CreateEntityMenu
+                    position={mode.position}
+                    dispatchMutation={dispatchMutation}
+                    onCancel={() => setMode({ type: "none" })}
+                />
             </Html>
         )
     }
---
+++ packages/web/src/app/editor/components/background/CreateEntityMenu.tsx
@@ -0,0 +1,66 @@
+import { EntityType } from "runtime/src/core/common/EntityType"
+import { entityModelRegistry } from "runtime/src/model/world/EntityModelRegistry"
+import { Point } from "runtime/src/model/world/Point"
+import { ContextMenu } from "../../../../common/components/ContextMenu"
+import { FlagSvg } from "../../../../common/components/inline-svg/Flag"
+import { RocketSvg } from "../../../../common/components/inline-svg/Rocket"
+import { TriangleSvg } from "../../../../common/components/inline-svg/Triangle"
+import { levelNew } from "../../entities/level/mutations/levelNew"
+import { rocketNew } from "../../entities/rocket/mutations/rocketNew"
+import { shapeNew } from "../../entities/shape/mutations/shapeNew"
+import { MutationGenerator } from "../../store/EditorStore"
+
+export function CreateEntityMenu(props: {
+    position: Point
+    dispatchMutation: (mutation: MutationGenerator) => void
+    onCancel: () => void
+}) {
+    function spawnPositionForType(position: Point, type: EntityType) {
+        return {
+            x: position.x - entityModelRegistry[type].width / 2,
+            y: position.y + entityModelRegistry[type].height / 2,
+        }
+    }
+
+    return (
+        <ContextMenu>
+            <li>
+                <a
+                    onClick={() => {
+                        props.dispatchMutation(shapeNew({ ...props.position }))
+                        props.onCancel()
+                    }}
+                >
+                    <TriangleSvg width="16" height="16" />
+                    Create Shape
+                </a>
+            </li>
+            <li>
+                <a
+                    onClick={() => {
+                        props.dispatchMutation(
+                            rocketNew(spawnPositionForType(props.position, EntityType.Rocket), 0),
+                        )
+                        props.onCancel()
+                    }}
+                >
+                    <RocketSvg width="16" height="16" />
+                    Create Rocket
+                </a>
+            </li>
+            <li>
+                <a
+                    onClick={() => {
+                        props.dispatchMutation(
+                            levelNew(spawnPositionForType(props.position, EntityType.Level), 0),
+                        )
+                        props.onCancel()
+                    }}
+                >
+                    <FlagSvg width="16" height="16" />
+                    Create Level
+                </a>
+            </li>
+        </ContextMenu>
+    )
+}

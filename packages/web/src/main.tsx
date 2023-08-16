ac331994b10efa70df4d3b63bb630557717ecb23
phisn <phisn@outlook.de>
2023-08-16 11:51:47 +0200

    Corretly enable application autoreload

--- packages/web/src/main.tsx
+++ packages/web/src/main.tsx
@@ -1,10 +1,12 @@
 import "./main.css"
 
+import React from "react"
 import { createRoot } from "react-dom/client"
 import { BrowserRouter } from "react-router-dom"
-
-import React from "react"
+import { registerSW } from "virtual:pwa-register"
 import App from "./app/App"
+
+registerSW({ immediate: true })
 
 createRoot(document.getElementById("root")!).render(
     <React.StrictMode>

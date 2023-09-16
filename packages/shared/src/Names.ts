4bf1219c87c84587bcab2ab654af02aedbddb5f9
phisn <phisn@outlook.de>
2023-09-16 16:57:32 +0200

    Add hardcoded names

--- packages/shared/src/Names.ts
+++ packages/shared/src/Names.ts
@@ -79,6 +79,17 @@ function hash(base: number, str: string) {
 }
 
 export function nameFromString(str: string) {
+    switch (str) {
+        case "mbq3wRsTxGAMttLDolJSDRpomLtC54QADMrIeKjfX+92NR260ob0BUzMRp4H8l5vpLLvifnOAoJTX2MK0PlW2A==":
+            return "Poccer77"
+        case "HXHp2V+dAgZJydaA6wIlSpSK16f/jfRrM4sW6ZDb2LoAXqe7GJI9naLByKjjRYfsY/j+BJA24qlvujibrElXWg==":
+            return "Monargoras"
+        case "AYFjDgH8bdKuKaP2oBbaPa8yeCmgtYc2DJSReD75W0QRK4RiWdc6LXHCGPamQW9F+Ncn4k76/UXqBlDqhRof3w==":
+            return "Phisn"
+        case "B+iYPOXE4ZVl61Yku20ayRq88bdPfQ3dmDhaCFWI203Y1Ui2KugbzcnubVptid8MZIjsnYRiUqYccCQOzu8pyg==":
+            return "Aelynna"
+    }
+
     const base1 = 0x211c9dc5
     const base2 = 0xf7aafd8a
 

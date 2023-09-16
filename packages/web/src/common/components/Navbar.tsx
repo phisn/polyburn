109dee480127208f21fde0cde8c55294c8e914f8
phisn <phisn@outlook.de>
2023-09-16 18:02:58 +0200

    Change playing navbar

--- packages/web/src/common/components/Navbar.tsx
+++ packages/web/src/common/components/Navbar.tsx
@@ -1,7 +1,7 @@
 // navbar with child content
 export function Navbar(props: { children: React.ReactNode }) {
     return (
-        <div className="bg-base-100 flex w-min items-center space-x-8 rounded-lg">
+        <div className="flex w-min items-center space-x-8 rounded-lg border-[1px] border-zinc-900 bg-black">
             {props.children}
         </div>
     )

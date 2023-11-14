07a585f236f31f937f88fb2e08316ed1b6c84eeb
phisn <phisn@outlook.de>
2023-11-14 19:55:23 +0100

    Continue prototyping

---
+++ packages/web/src/app/editor-concept-5/behaviours/object/use-object-bahavior.ts
@@ -0,0 +1,14 @@
+import { useState } from "react"
+import { useListenerRef } from "../../use-listener"
+
+export function useObjectBehavior(props: {
+    isInside: (x: number, y: number) => boolean
+    setPosition: (x: number, y: number) => void
+}) {
+    const isInsideRef = useListenerRef(props.isInside)
+    const setPositionRef = useListenerRef(props.setPosition)
+
+    const [isHovered, setIsHovered] = useState(false)
+
+    useEventListener(event => {})
+}
--- packages/web/src/app/editor-concept-5/entities/rocket/Rocket.tsx
+++ packages/web/src/app/editor-concept-5/entities/rocket/Rocket.tsx
@@ -1,10 +1,52 @@
-export function Rocket() {
-    // do a 1 to 1 mapping between behavior and visual
-    // therefore the behavior is handling all the visual stuff
+import { Svg } from "@react-three/drei"
+import { Suspense, useRef } from "react"
+import { EntityType } from "runtime/proto/world"
+import { Euler, MeshBasicMaterial } from "three"
+import { entityGraphicRegistry } from "../../../../game/runtime-view/graphics-assets/entity-graphic-registry"
+import { EntityGraphicType } from "../../../../game/runtime-view/graphics-assets/entity-graphic-type"
+import { isPointInsideEntity } from "../../../editor/models/is-point-inside-entity"
+import { resolveEntityOrder } from "../resolve-entity-order"
+
+export function Rocket(props: { entity: RocketEntity }) {
+    const svgRef = useRef<Object3D>(null)
+    const graphicEntry = entityGraphicRegistry[EntityGraphicType.Rocket]
+
+    const { hovered } = useObjectBehavior({
+        isInside: point =>
+            isPointInsideEntity(
+                point,
+                props.entity.position,
+                props.entity.rotation,
+                EntityGraphicType.Rocket,
+            ),
+        setPosition: (position, rotation) => {
+            svgRef.current!.position.set(
+                position.x,
+                position.y,
+                resolveEntityOrder({ inAction: true }),
+            )
+            svgRef.current!.rotation.set(0, 0, rotation)
+        },
+    })
 
     return (
-        <ObjectBehavior>
-            <Svg ref={svgRef as any} />
-        </ObjectBehavior>
+        <Suspense>
+            <Svg
+                ref={svgRef as any}
+                position={[
+                    props.state.position.x,
+                    props.state.position.y,
+                    resolveEntityOrder({
+                        entityType: EntityType.ROCKET,
+                        hovered,
+                        selected: false,
+                    }),
+                ]}
+                rotation={new Euler(0, 0, props.state.rotation)}
+                src={graphicEntry.src}
+                scale={graphicEntry.scale}
+                fillMaterial={hovered ? new MeshBasicMaterial({ color: "#ffff55" }) : undefined}
+            />
+        </Suspense>
     )
 }

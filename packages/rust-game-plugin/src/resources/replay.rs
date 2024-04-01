362d350048d28296e6d84fc8e4f550b3f652dc0a
phisn <phisn@outlook.de>
2024-04-02 01:17:31 +0200

    Remove not needed tag

--- packages/rust-game-plugin/src/resources/replay.rs
+++ packages/rust-game-plugin/src/resources/replay.rs
@@ -4,7 +4,6 @@ use serde::{Deserialize, Serialize};
 pub struct Replay;
 
 #[derive(Resource, Event, Clone, Copy, Serialize, Deserialize, Default)]
-#[wasm_bindgen]
 pub struct FrameInput {
     pub rotation: f32,
     pub thrust: bool,

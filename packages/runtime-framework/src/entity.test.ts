d7adba599614b33a50eb0a7944403c993c7eea6b
phisn <phisn@outlook.de>
2024-06-16 13:04:45 +0200

    Refactor and reprototype runtime framework

--- packages/runtime-framework/src/entity.test.ts
+++ packages/runtime-framework/src/entity.test.ts
@@ -1,5 +1,5 @@
 import { beforeEach, describe, expect, it } from "vitest"
-import { EntityStore, newEntityStore } from "./entity"
+import { EntityStore, newEntityStore2 } from "./entity"
 
 interface Components {
     position: { x: number; y: number }
@@ -10,10 +10,11 @@ interface Components {
 let store: EntityStore<Components>
 
 beforeEach(() => {
-    store = newEntityStore<Components>()
+    store = newEntityStore2<Components>()
 })
 
 describe("EntityStore", () => {
+    /*
     it("should create and retrieve a single entity with specified components", () => {
         const entity = store.create({ position: { x: 0, y: 0 } })
         const retrieved = store.single("position")
@@ -37,29 +38,29 @@ describe("EntityStore", () => {
         const entity1 = store.create({ position: { x: 0, y: 0 } })
         const entity2 = store.create({ position: { x: 1, y: 1 } })
         const retrieved = store.multiple("position")
-        expect(retrieved.length).toBe(2)
-        expect(retrieved[0].position).toEqual(entity1.position)
-        expect(retrieved[1].position).toEqual(entity2.position)
+        expect(retrieved.size).toBe(2)
+        expect([...retrieved][0].position).toEqual(entity1.position)
+        expect([...retrieved][1].position).toEqual(entity2.position)
     })
 
     it("should update multiple entities list when new entity is created", () => {
         const entity1 = store.create({ position: { x: 0, y: 0 } })
         const retrieved = store.multiple("position")
-        expect(retrieved.length).toBe(1)
-        expect(retrieved[0].position).toEqual(entity1.position)
+        expect(retrieved.size).toBe(1)
+        expect([...retrieved][0].position).toEqual(entity1.position)
 
         const entity2 = store.create({ position: { x: 1, y: 1 } })
         const newRetrieved = store.multiple("position")
 
         expect(newRetrieved).toBe(retrieved)
 
-        expect(newRetrieved.length).toBe(2)
-        expect(newRetrieved[0].position).toEqual(entity1.position)
-        expect(newRetrieved[1].position).toEqual(entity2.position)
+        expect(newRetrieved.size).toBe(2)
+        expect([...newRetrieved][0].position).toEqual(entity1.position)
+        expect([...newRetrieved][1].position).toEqual(entity2.position)
 
-        expect(retrieved.length).toBe(2)
-        expect(retrieved[0].position).toEqual(entity1.position)
-        expect(retrieved[1].position).toEqual(entity2.position)
+        expect(retrieved.size).toBe(2)
+        expect([...retrieved][0].position).toEqual(entity1.position)
+        expect([...retrieved][1].position).toEqual(entity2.position)
 
         expect(retrieved).toBe(newRetrieved)
     })
@@ -69,17 +70,17 @@ describe("EntityStore", () => {
         const entity2 = store.create({ position: { x: 1, y: 1 } })
         const retrieved = store.multiple("position")
 
-        expect(retrieved.length).toBe(2)
-        expect(retrieved[0].position).toEqual(entity1.position)
-        expect(retrieved[1].position).toEqual(entity2.position)
+        expect(retrieved.size).toBe(2)
+        expect([...retrieved][0].position).toEqual(entity1.position)
+        expect([...retrieved][1].position).toEqual(entity2.position)
 
         store.remove(entity1)
 
         const newRetrieved = store.multiple("position")
         expect(newRetrieved).toBe(retrieved)
 
-        expect(retrieved.length).toBe(1)
-        expect(retrieved[0].position).toEqual(entity2.position)
+        expect(retrieved.size).toBe(1)
+        expect([...retrieved][0].position).toEqual(entity2.position)
     })
 
     it("should allow setting components for an entity", () => {
@@ -125,19 +126,20 @@ describe("EntityStore", () => {
             store.multiple("position", "velocity", "not-health"),
         ]
 
+        console.log(multiple)
+
         for (const retrieved of multiple) {
-            expect(retrieved.length).toBe(1)
-            expect(retrieved[0].position).toBeDefined()
-            expect(retrieved[0].velocity).toBeDefined()
+            expect(retrieved.size).toBe(1)
+            expect([...retrieved][0].position).toBeDefined()
+            expect([...retrieved][0].velocity).toBeDefined()
         }
 
         store.create({ position: { x: 0, y: 0 }, velocity: { dx: 1, dy: 1 } })
 
         for (const retrieved of multiple) {
-            console.log("NEXT")
-            expect(retrieved.length).toBe(2)
-            expect(retrieved[0].position).toBeDefined()
-            expect(retrieved[0].velocity).toBeDefined()
+            expect(retrieved.size).toBe(2)
+            expect([...retrieved][0].position).toBeDefined()
+            expect([...retrieved][0].velocity).toBeDefined()
         }
     })
 
@@ -163,7 +165,7 @@ describe("EntityStore", () => {
 
         // Verify the remaining entities
         const remainingEntities = store.multiple("position", "velocity", "health")
-        expect(remainingEntities.length).toBe(250)
+        expect(remainingEntities.size).toBe(250)
 
         // Perform random updates on remaining entities
         remainingEntities.forEach(entity => {
@@ -193,9 +195,9 @@ describe("EntityStore", () => {
 
         const retrieved = store.multiple("not-velocity")
 
-        expect(retrieved.length).toBe(1)
-        expect(retrieved[0].position).toEqual(entity2.position)
-        expect(retrieved[0].health).toEqual(entity2.health)
+        expect(retrieved.size).toBe(1)
+        expect([...retrieved][0].position).toEqual(entity2.position)
+        expect([...retrieved][0].health).toEqual(entity2.health)
     })
 
     it("should retrieve entities with multiple specified components and absence of others", () => {
@@ -205,9 +207,9 @@ describe("EntityStore", () => {
 
         const retrieved = store.multiple("position", "not-velocity")
 
-        expect(retrieved.length).toBe(1)
-        expect(retrieved[0].position).toEqual(entity2.position)
-        expect(retrieved[0].health).toEqual(entity2.health)
+        expect(retrieved.size).toBe(1)
+        expect([...retrieved][0].position).toEqual(entity2.position)
+        expect([...retrieved][0].health).toEqual(entity2.health)
     })
 
     it("should retrieve a single entity with a specified component and absence of another", () => {
@@ -234,6 +236,7 @@ describe("EntityStore", () => {
         const single = store.single("position", "not-velocity")
         expect(() => single.position).toThrow()
     })
+    */
 
     it("should allow singles with only negative", () => {
         const entity = store.create({ position: { x: 0, y: 0 } })
@@ -243,8 +246,19 @@ describe("EntityStore", () => {
 
         const single2 = store.single("not-health")
         const entity2 = store.create({ velocity: { dx: 1, dy: 1 } })
+
+        expect(() => single2.position).toThrow()
+        expect(() => single2.velocity).toThrow()
+
+        expect(single.position).toEqual(entity.position)
+
+        entity.health = { hp: 100 }
+
+        expect(single.position).toEqual(entity.position)
         expect(single2.velocity).toEqual(entity2.velocity)
     })
+
+    return
 
     it("should record entities changing correctly", () => {
         store.create({ position: { x: 0, y: 0 } })
@@ -255,42 +269,49 @@ describe("EntityStore", () => {
 
         const changing2 = store.changing("not-velocity")
 
-        expect(changes.length).toBe(2)
-        expect(changes[0].type).toBe("created")
-        expect(changes[0].entity.position).toEqual({ x: 0, y: 0 })
-        expect(changes[1].type).toBe("created")
-        expect(changes[1].entity.position).toEqual({ x: 1, y: 1 })
+        console.log(changes)
+
+        expect(changes.added.size).toBe(2)
+        expect(changes.removed.size).toBe(0)
+        expect([...changes.added][0].position).toEqual({ x: 0, y: 0 })
+        expect([...changes.added][1].position).toEqual({ x: 1, y: 1 })
 
         const b = store.create({ position: { x: 2, y: 2 } })
         store.create({ position: { x: 3, y: 3 }, velocity: { dx: 3, dy: 3 } })
 
         const newChanges = changing()
 
-        expect(newChanges.length).toBe(2)
-        expect(newChanges[0].type).toBe("created")
-        expect(newChanges[0].entity.position).toEqual({ x: 2, y: 2 })
-        expect(newChanges[1].type).toBe("created")
-        expect(newChanges[1].entity.position).toEqual({ x: 3, y: 3 })
+        expect(newChanges.added.size).toBe(2)
+        expect(newChanges.removed.size).toBe(0)
+        expect([...newChanges.added][0].position).toEqual({ x: 2, y: 2 })
+        expect([...newChanges.added][1].position).toEqual({ x: 3, y: 3 })
 
         store.remove(a)
         store.remove(b)
 
         const removedChanges = changing()
 
-        expect(removedChanges.length).toBe(2)
-        expect(removedChanges[0].type).toBe("removed")
-        expect(removedChanges[0].entity.position).toEqual({ x: 1, y: 1 })
-        expect(removedChanges[1].type).toBe("removed")
-        expect(removedChanges[1].entity.position).toEqual({ x: 2, y: 2 })
+        expect(removedChanges.added.size).toBe(0)
+        expect(removedChanges.removed.size).toBe(2)
+        expect([...removedChanges.removed][0].position).toEqual({ x: 1, y: 1 })
+        expect([...removedChanges.removed][1].position).toEqual({ x: 2, y: 2 })
 
         const changes2 = changing2()
 
-        expect(changes2.length).toBe(3)
-        expect(changes2[0].type).toBe("created")
-        expect(changes2[0].entity.position).toEqual({ x: 0, y: 0 })
-        expect(changes2[1].type).toBe("created")
-        expect(changes2[1].entity.position).toEqual({ x: 2, y: 2 })
-        expect(changes2[2].type).toBe("removed")
-        expect(changes2[2].entity.position).toEqual({ x: 2, y: 2 })
+        /*
+        expect(changes2.size).toBe(3)
+        expect([...changes2][0].type).toBe("created")
+        expect([...changes2][0].entity.position).toEqual({ x: 0, y: 0 })
+        expect([...changes2][1].type).toBe("created")
+        expect([...changes2][1].entity.position).toEqual({ x: 2, y: 2 })
+        expect([...changes2][2].type).toBe("removed")
+        expect([...changes2][2].entity.position).toEqual({ x: 2, y: 2 })
+        */
+
+        expect(changes2.added.size).toBe(2)
+        expect(changes2.removed.size).toBe(1)
+        expect([...changes2.added][0].position).toEqual({ x: 0, y: 0 })
+        expect([...changes2.added][1].position).toEqual({ x: 3, y: 3 })
+        expect([...changes2.removed][0].position).toEqual({ x: 1, y: 1 })
     })
 })
--- packages/runtime-framework/src/entity.ts
+++ packages/runtime-framework/src/entity.ts
@@ -6,13 +6,10 @@ export type EntityWith<Components extends object, C extends keyof Components> = 
 
 const ModifierNot = "not-"
 
-const Modifiers = [ModifierNot] as const
-type Modifiers = (typeof Modifiers)[number]
-
 // => keyof Components & "not-" + keyof Components
 type ComponentsWithModifier<Components extends object> =
     | keyof Components
-    | `${Modifiers}${keyof Components & string}`
+    | `${typeof ModifierNot}${keyof Components & string}`
 
 // filter the result of ComponentsWithModifier so that it is keyof Components
 type FilterModifier<
@@ -21,8 +18,8 @@ type FilterModifier<
 > = C extends keyof Components ? C : never
 
 export interface EntityChange<Components extends object, C extends keyof Components> {
-    type: "created" | "removed"
-    entity: Readonly<EntityWith<Components, C>>
+    added: ReadonlySet<EntityWith<Components, C>>
+    removed: ReadonlySet<EntityWith<Components, C>>
 }
 
 export interface EntityStore<Components extends object> {
@@ -35,23 +32,11 @@ export interface EntityStore<Components extends object> {
 
     multiple<C extends ComponentsWithModifier<Components>[]>(
         ...components: C
-    ): readonly EntityWith<Components, FilterModifier<C[number], Components>>[]
+    ): ReadonlySet<EntityWith<Components, FilterModifier<C[number], Components>>>
 
     changing<K extends ComponentsWithModifier<Components>[]>(
         ...components: K
-    ): () => EntityChange<Components, FilterModifier<K[number], Components>>[]
-
-    // works using a weak set. if the callee does not reference the function it will be garbage collected
-    // will return all new entities created/removed since the last call
-
-    /*
-    created<K extends ComponentsWithModifier<Components>[]>(
-        ...components: K
-    ): () => EntityWith<Components, FilterModifier<K[number], Components>>[]
-    removed<K extends ComponentsWithModifier<Components>[]>(
-        ...components: K
-    ): () => EntityWith<Components, FilterModifier<K[number], Components>>[]
-    */
+    ): () => EntityChange<Components, FilterModifier<K[number], Components>>
 }
 
 class WeakList<T extends WeakKey> {
@@ -63,10 +48,13 @@ class WeakList<T extends WeakKey> {
 
     [Symbol.iterator]() {
         this.clean()
-
         const values = this.list.map(x => x.deref()).filter(x => x !== undefined) as T[]
-
         return values[Symbol.iterator]()
+    }
+
+    get values() {
+        this.clean()
+        return this.list.map(x => x.deref()).filter(x => x !== undefined) as T[]
     }
 
     get length() {
@@ -248,6 +236,12 @@ class EntityChangeCache {
 
         for (const bucket of created) {
             for (const listener of bucket) {
+                const exists = listener.findIndex(change => change.entity === entity)
+
+                if (exists) {
+                    listener.splice(exists, 1)
+                }
+
                 listener.push({ type: "created", entity: { ...entity } })
             }
         }
@@ -289,6 +283,7 @@ export function newEntityStore<Components extends object>() {
         const entity: any = new Proxy(
             {
                 __id: nextId++,
+                ...components,
             },
             {
                 set(target: any, key: string, value) {
@@ -320,9 +315,11 @@ export function newEntityStore<Components extends object>() {
 
         entities.set(entity.__id, entity)
 
+        /*
         for (const component of componentNames) {
             entity[component] = components[component]
         }
+        */
 
         return entity
     }
@@ -405,14 +402,6 @@ export function newEntityStore<Components extends object>() {
         const componentsRequired = componentsWithModifier.filter(x => !x.startsWith("not-"))
         const componentsForbidden = filterComponentsWithModifier("not-")
 
-        // sanity check that does not really impact performance
-        if (
-            componentsRequired.filter(x => Modifiers.some(modifer => x.startsWith(modifer)))
-                .length > 0
-        ) {
-            throw new Error("Unhandeled modifier")
-        }
-
         const newEntityList: any[] = []
         entityListKeyCache.insert(newEntityList, componentsWithModifier)
 
@@ -454,4 +443,413 @@ export function newEntityStore<Components extends object>() {
     }
 
     return { create, remove, single, multiple, changing }
+}
+
+class WeakList2<T extends WeakKey> {
+    private list: WeakRef<T>[] = []
+
+    push(value: T) {
+        this.list.push(new WeakRef(value))
+    }
+
+    *values() {
+        let dirty = false
+
+        for (const item of this.list) {
+            const value = item.deref()
+
+            if (value === undefined) {
+                dirty = true
+                continue
+            }
+
+            yield value
+        }
+
+        if (dirty) {
+            this.list = this.list.filter(x => x.deref() !== undefined)
+        }
+    }
+}
+
+export function newEntityStore2<Components extends object>(): EntityStore<Components> {
+    const forbiddenKeys = ["__id", "__key", "__archeType", "__internal", "__entity", ModifierNot]
+
+    function componentsToKey(components: string[]) {
+        return components.sort().join(",")
+    }
+
+    function requirementsSatisfyComponents(requirements: string[], components: string[]) {
+        for (const requirement of requirements) {
+            if (requirement.startsWith(ModifierNot)) {
+                const forbidden = requirement.slice(ModifierNot.length)
+
+                if (components.includes(forbidden)) {
+                    return false
+                }
+
+                continue
+            }
+
+            if (components.includes(requirement) === false) {
+                return false
+            }
+        }
+
+        return true
+    }
+
+    function populateArcheTypeList(archeType: EntityArcheType, list: EntityList) {
+        archeType.lists.push(list)
+
+        for (const requirement of list.requirements) {
+            let componentToList = archeType.componentToLists.get(requirement)
+
+            if (componentToList === undefined) {
+                componentToList = []
+                archeType.componentToLists.set(requirement, componentToList)
+            }
+
+            componentToList.push(list)
+        }
+    }
+
+    function populateArcheTypeChangeListener(
+        archeType: EntityArcheType,
+        changeListener: ChangeListener,
+    ) {
+        archeType.changeListeners.push(changeListener)
+
+        for (const requirement of changeListener.requirements) {
+            let componentToChangeListener = archeType.componentToChangeListeners.get(requirement)
+
+            if (componentToChangeListener === undefined) {
+                componentToChangeListener = new WeakList2()
+                archeType.componentToChangeListeners.set(requirement, componentToChangeListener)
+            }
+
+            componentToChangeListener.push(changeListener)
+        }
+    }
+
+    function emplaceEntityArcheType(key: string, components: string[]) {
+        let archeType = entityArcheTypes.get(key)
+
+        if (archeType === undefined) {
+            archeType = {
+                components,
+                entities: new Set<EntityInternal>(),
+
+                componentToLists: new Map(),
+                componentToChangeListeners: new Map(),
+
+                lists: [],
+                changeListeners: new WeakList2(),
+            }
+
+            const lists = [...keyToEntityList.values()].filter(x =>
+                requirementsSatisfyComponents(x.requirements, components),
+            )
+
+            for (const list of lists) {
+                populateArcheTypeList(archeType, list)
+            }
+
+            for (const changeListener of entityChangeListeners.values()) {
+                if (requirementsSatisfyComponents(changeListener.requirements, components)) {
+                    populateArcheTypeChangeListener(archeType, changeListener)
+                }
+            }
+
+            entityArcheTypes.set(key, archeType)
+        }
+
+        return archeType
+    }
+
+    function resolveEntity(entityToResolve: any): EntityInternal {
+        const id = entityToResolve.__id
+
+        if (id === undefined) {
+            throw new Error("Invalid entity")
+        }
+
+        const entity = entities.get(id)
+
+        if (entity === undefined) {
+            throw new Error("Entity does not exist")
+        }
+
+        return entity
+    }
+
+    // signals that a property changed from existing to not existing or vice versa
+    function entityPropertyChanged(entity: EntityInternal, from: string, to: string) {
+        const toRemove = entity.__archeType.componentToLists.get(from)
+
+        if (toRemove) {
+            for (const list of toRemove) {
+                list.entities.delete(entity.__entity)
+            }
+        }
+
+        const notifyRemove = entity.__archeType.componentToChangeListeners.get(from)
+
+        if (notifyRemove) {
+            for (const listener of notifyRemove.values()) {
+                if (listener.added.has(entity.__entity)) {
+                    listener.added.delete(entity.__entity)
+                } else {
+                    listener.removed.add(entity.__entity)
+                }
+            }
+        }
+
+        entity.__archeType.entities.delete(entity.__entity)
+
+        const key = componentsToKey(Object.getOwnPropertyNames(entity))
+
+        entity.__key = key
+        entity.__archeType = emplaceEntityArcheType(key, Object.getOwnPropertyNames(entity))
+        entity.__archeType.entities.add(entity.__entity)
+
+        const toAdd = entity.__archeType.componentToLists.get(to)
+
+        if (toAdd) {
+            for (const list of toAdd) {
+                list.entities.add(entity.__entity)
+            }
+        }
+
+        const notifyAdd = entity.__archeType.componentToChangeListeners.get(to)
+
+        if (notifyAdd) {
+            for (const listener of notifyAdd.values()) {
+                if (listener.removed.has(entity.__entity)) {
+                    listener.removed.delete(entity.__entity)
+                } else {
+                    listener.added.add(entity.__entity)
+                }
+            }
+        }
+    }
+
+    type EntityInternal = {
+        __internal: EntityInternal
+        __entity: EntityInternal
+        __id: number
+        __key: string
+        __archeType: EntityArcheType
+    } & Record<string, any>
+
+    interface EntityList {
+        entities: Set<EntityInternal>
+        requirements: string[]
+    }
+
+    interface ChangeListener {
+        requirements: string[]
+
+        added: Set<EntityInternal>
+        removed: Set<EntityInternal>
+    }
+
+    interface EntityArcheType {
+        components: string[]
+        entities: Set<EntityInternal>
+
+        componentToLists: Map<string, EntityList[]>
+        componentToChangeListeners: Map<string, WeakList2<ChangeListener>>
+
+        lists: EntityList[]
+        changeListeners: WeakList2<ChangeListener>
+    }
+
+    let nextEntityId = 0
+
+    const entityArcheTypes = new Map<string, EntityArcheType>()
+    const entityChangeListeners = new WeakList2<ChangeListener>()
+
+    const keyToEntityList = new Map<string, EntityList>()
+    const keyToSingle = new Map<string, any>()
+
+    const entityProxyHandler: ProxyHandler<EntityInternal> = {
+        set(target, p: string, newValue, receiver) {
+            const exists = Reflect.has(target, p)
+            const result = Reflect.set(target, p, newValue, receiver)
+
+            if (exists) {
+                return result
+            }
+
+            entityPropertyChanged(target, ModifierNot + p, p)
+
+            return result
+        },
+        deleteProperty(target, p: string) {
+            const result = Reflect.deleteProperty(target, p)
+
+            if (result === false) {
+                return false
+            }
+
+            entityPropertyChanged(target, p, ModifierNot + p)
+
+            return result
+        },
+    }
+
+    function create<K extends keyof Components>(base: any): EntityWith<Components, K> {
+        const components = Object.getOwnPropertyNames(base)
+        const key = componentsToKey(components)
+
+        if (components.some(x => forbiddenKeys.includes(x))) {
+            throw new Error("Invalid component name")
+        }
+
+        base.__internal = base
+        base.__id = nextEntityId++
+        base.__key = key
+        base.__archeType = emplaceEntityArcheType(key, components)
+
+        const entity: EntityInternal = new Proxy(base, entityProxyHandler)
+
+        base.__entity = entity
+
+        for (const list of entity.__archeType.lists) {
+            list.entities.add(entity.__entity)
+        }
+
+        for (const changeListener of entity.__archeType.changeListeners.values()) {
+            changeListener.added.add(entity.__entity)
+        }
+
+        base.__archeType.entities.add(entity)
+
+        return entity as EntityWith<Components, K>
+    }
+
+    function remove(entityToResolve: any): void {
+        const entity = entityToResolve as EntityInternal
+
+        for (const list of entity.__archeType.lists) {
+            list.entities.delete(entity.__entity)
+        }
+
+        for (const changeListener of entity.__archeType.changeListeners.values()) {
+            changeListener.removed.add({ ...entity.__internal })
+        }
+
+        entity.__archeType.entities.delete(entity.__entity)
+    }
+
+    function multiple<C extends ComponentsWithModifier<Components>[]>(
+        ...requirements: string[]
+    ): ReadonlySet<EntityWith<Components, FilterModifier<C[number], Components>>> {
+        const key = componentsToKey(requirements)
+        const list = keyToEntityList.get(key)
+
+        if (list) {
+            return list.entities as any
+        }
+
+        const entities = new Set<EntityInternal>()
+        const newList: EntityList = { entities, requirements }
+
+        keyToEntityList.set(key, newList)
+
+        for (const archeType of entityArcheTypes.values()) {
+            if (requirementsSatisfyComponents(requirements, archeType.components)) {
+                for (const entity of archeType.entities) {
+                    entities.add(entity)
+                }
+
+                populateArcheTypeList(archeType, newList)
+            }
+        }
+
+        return entities as any
+    }
+
+    function single<C extends ComponentsWithModifier<Components>[]>(
+        ...requirements: string[]
+    ): EntityWith<Components, FilterModifier<C[number], Components>> {
+        const key = componentsToKey(requirements)
+        const existing = keyToSingle.get(key)
+
+        if (existing) {
+            return existing
+        }
+
+        const list: ReadonlySet<any> = multiple<C>(...requirements)
+
+        function ensureSingleEntity() {
+            if (list.size < 1) {
+                throw new Error(
+                    `Single entity with components "${requirements.join(", ")}" does not exist`,
+                )
+            }
+
+            if (list.size > 1) {
+                throw new Error(
+                    `Single entity with components "${requirements.join(", ")}" is not unique`,
+                )
+            }
+
+            const [x] = list
+            return x
+        }
+
+        const newSingle = new Proxy(
+            {},
+            {
+                get: (_, prop: string) => ensureSingleEntity()[prop],
+                set: (_, prop: string, value) => (ensureSingleEntity()[prop] = value),
+                deleteProperty: (_, prop: string) => delete ensureSingleEntity()[prop],
+                ownKeys: () => Reflect.ownKeys(ensureSingleEntity()),
+            },
+        )
+
+        keyToSingle.set(key, newSingle)
+
+        return newSingle as any
+    }
+
+    function changing(...requirements: string[]) {
+        const changeListener: ChangeListener = {
+            requirements,
+            added: new Set(),
+            removed: new Set(),
+        }
+
+        for (const archeType of entityArcheTypes.values()) {
+            if (requirementsSatisfyComponents(requirements, archeType.components)) {
+                for (const entity of archeType.entities) {
+                    changeListener.added.add(entity)
+                }
+
+                populateArcheTypeChangeListener(archeType, changeListener)
+            }
+        }
+
+        entityChangeListeners.push(changeListener)
+
+        return () => {
+            const added = changeListener.added
+            const removed = changeListener.removed
+
+            changeListener.added = new Set()
+            changeListener.removed = new Set()
+
+            return { added, removed }
+        }
+    }
+
+    return {
+        create,
+        remove,
+        multiple: multiple as any,
+        single: single as any,
+        changing: changing as any,
+    }
 }

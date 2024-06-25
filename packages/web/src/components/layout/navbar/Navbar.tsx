1910c7ac8a5d668335864c9b0aeb26ea2bddc03d
phisn <phisn@outlook.de>
2024-06-25 12:35:23 +0200

    Hide github text when no space is available

--- packages/web/src/components/layout/navbar/Navbar.tsx
+++ packages/web/src/components/layout/navbar/Navbar.tsx
@@ -15,7 +15,7 @@ export function Navbar() {
                 onClick={() => window.open("https://github.com/phisn/polyburn")}
             >
                 <GithubSvg width="24" height="24" />
-                <div>Github</div>
+                <div className="xs:flex hidden">Github</div>
                 {/*<BackArrowSvg width="48" height="48" />*/}
             </div>
             <div className="justify-self-center">

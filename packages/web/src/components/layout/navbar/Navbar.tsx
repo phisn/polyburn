2f51a5ba2f44a8352240ffcaaa5e138d7ba77952
phisn <phisn@outlook.de>
2024-06-25 12:27:40 +0200

    Add github to navbar

--- packages/web/src/components/layout/navbar/Navbar.tsx
+++ packages/web/src/components/layout/navbar/Navbar.tsx
@@ -1,5 +1,5 @@
 import { isMobile } from "react-device-detect"
-import { BackArrowSvg } from "../../common/svg/BackArrow"
+import { GithubSvg } from "../../common/svg/Github"
 import { NavbarAuthButton } from "./NavbarAuthButton"
 import { Title } from "./Title"
 
@@ -10,8 +10,13 @@ export function Navbar() {
         <div
             className={`bg-base-300 relative z-10 grid grid-cols-3 items-center bg-opacity-50 px-4 backdrop-blur-sm ${mobileClass}`}
         >
-            <div className="btn btn-lg btn-ghost btn-square invisible">
-                <BackArrowSvg width="48" height="48" />
+            <div
+                className="btn btn-lg btn-ghost w-fit space-x-4"
+                onClick={() => window.open("https://github.com/phisn/polyburn")}
+            >
+                <GithubSvg width="24" height="24" />
+                <div>Github</div>
+                {/*<BackArrowSvg width="48" height="48" />*/}
             </div>
             <div className="justify-self-center">
                 <Title />
